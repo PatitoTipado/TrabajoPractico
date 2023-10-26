0bf9044fdfee96eda0dd39bb2193202175bd9ff4
NaN <140985447+PatitoTipado@users.noreply.github.com>
2023-10-26 18:26:01 -0300

    TrabajoPracticoN5

---
+++ trabajoPractico5/src/ar/edu/unlam/pb1/rewind/Persona.java
@@ -0,0 +1,68 @@
+package ar.edu.unlam.pb1.rewind;
+
+public class Persona {
+	private String nombre;
+	private String genero;
+	private long dni;
+	private double peso;
+	private double altura;
+	private int edad;
+	private boolean viva; //por que me sale que no esta siendo usada?
+
+	public Persona(String nombre, String genero, long dni, double peso, double altura) {
+		this.nombre = nombre;
+		this.genero = genero;
+		this.dni = dni;
+		this.peso = peso;
+		this.altura = altura;
+		this.viva = true;
+	}
+
+	public void alimentar(double kilos) {
+		if (this.viva) {
+			this.peso += kilos;
+		}
+	}
+
+	public void crecer(double metros) {
+		if (this.viva) {
+			this.altura += metros;
+		}
+	}
+
+	public void cumpliranios() {
+		this.edad++;
+	}
+
+	public void morir() {
+		this.viva = false;
+	}
+
+	public double pesar() {
+		return peso;
+	}
+
+	public double medir() {
+		return altura;
+	}
+	public boolean esMayorDeEdad() {
+		return this.edad>18;
+	}
+	public boolean estaViva() {
+		return viva;
+	}
+	public void cumplirVarios(int aniosCumplir) {
+		if(aniosCumplir>0) {
+			for (int i = 0; i < aniosCumplir; i++) {
+				edad++;
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", genero=" + genero + ", dni=" + dni + ", peso=" + peso + ", altura="
+				+ altura + ", edad=" + edad + "]";
+	}
+
+}
---
+++ trabajoPractico5/src/ar/edu/unlam/pb1/rewind/Test.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.pb1.rewind;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculadora casio=new Calculadora();
+		System.out.println(casio.contarDivisores(10));
+	}
+
+}
